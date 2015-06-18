89fca39b756825bee5fa2dd8519fba7a1f3aa8af
Phillipe Caixeta Rodrigues <caixeta.phillipe@gmail.com>
2015-06-17 21:20:14 -0300

    Finalizando!
    Ufa! Finalmente!

--- server/app.js
+++ server/app.js
@@ -1,29 +1,60 @@
 var express = require('express'),
     app = express();
 
+var _ = require('underscore');
+
 // carregar "banco de dados" (data/jogadores.json e data/jogosPorJogador.json)
-// dica: 3-4 linhas de código (você deve usar o módulo de filesystem (fs))
-var db = {
-};
+fs = require('fs');
+var jogadores = JSON.parse(fs.readFileSync(__dirname + '/data/jogadores.json'));
+var jogosxjogadores = JSON.parse(fs.readFileSync(__dirname + '/data/jogosPorJogador.json'));
+var db = _.extend({}, JSON.parse(fs.readFileSync(__dirname + "/data/jogadores.json")),
+    JSON.parse(fs.readFileSync(__dirname + "/data/jogosPorJogador.json")));
 
 
-// configurar qual templating engine usar. Sugestão: hbs (handlebars)
-//app.set('view engine', '???');
+// configurar qual templating engine usar.
+app.set('view engine', 'hbs');
 
 
 // definir rota para página inicial --> renderizar a view index, usando os
 // dados do banco de dados "data/jogadores.json" com a lista de jogadores
-// dica: o handler desta função é bem simples - basta passar para o template
-//       os dados do arquivo data/jogadores.json
+app.set('views', 'server/views');
 
 // definir rota para página de detalhes de um jogador --> renderizar a view
 // jogador, usando os dados do banco de dados "data/jogadores.json" e
 // "data/jogosPorJogador.json", assim como alguns campos calculados
-// dica: o handler desta função pode chegar a ter umas 15 linhas de código
+app.get('/',function (req, res) {
+    res.render('index',db, function(err, html) {
+                                  res.send(html);
+                                });
+});
+app.get('/jogador/:id/',function (req, res) {
+    var jogador = _.find(db.players, function (el) { return el.steamid == req.params.id; });
+    var games = _.map(_.sortBy(db[req.params.id].games, function (g) { return -g.playtime_forever; }), function (game) {
+        return {
+            appid: game.appid,
+            name: game.name,
+            playtime_forever: game.playtime_forever / 60 | 0,
+            img_logo_url: game.img_logo_url
+        };
+    });;
+
+    res.render('jogador', {
+        steamid: jogador.steamid,
+        avatar: jogador.avatar,
+        personname: jogador.personaname,
+        loccountrycode: jogador.loccountrycode,
+        realname: jogador.realname,
+        profilename: jogador.personaname,
+        numOfGames: db[req.params.id].game_count,
+        numOfNonPlayedGames: _.where(db[req.params.id].games, { playtime_forever: 0 }).length,
+        favoriteGame: games[0],
+        topFiveGames: games.slice(0, 5)
+    });
+});
 
 
 // configurar para servir os arquivos estáticos da pasta "client"
-// dica: 1 linha de código
+app.use(express.static('client'));
 
 // abrir servidor
-// dica: 1-3 linhas de código
+app.listen(3000);
